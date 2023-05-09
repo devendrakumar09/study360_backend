0f9908ebbb154235e8167e2a84ec02d878c900ed
Devendra Kumar <cktdevendra955@gmail.com>
2023-05-09 15:32:16 +0530

    Practice Set On Going

---
+++ practiceSet/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+class AnswerAdmin(admin.StackedInline):
+    model = Answers
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdmin]
+
+admin.site.register(Categories)
+admin.site.register(Set)
+admin.site.register(Queston,QuestionAdmin)
+admin.site.register(Answers)
---
+++ practiceSet/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+import uuid
+# Create your models here.
+
+class Categories(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)   
+    title = models.CharField(max_length=250,null=False)
+    created_at = models.DateTimeField(auto_now=True,)
+    updated_at = models.DateTimeField(auto_now=True,)
+    deleted_at = models.DateTimeField(auto_now=True,)
+    
+    def __str__(self):
+        return self.title   
+  
+
+class Set(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    category = models.ForeignKey("Categories", related_name="category_set", on_delete=models.CASCADE)
+    title = models.CharField(max_length=250,null=False)
+    description = models.CharField(max_length=50,null=False)
+    created_at = models.DateTimeField(auto_now=True,)
+    updated_at = models.DateTimeField(auto_now=True,)
+    deleted_at = models.DateTimeField(auto_now=True,)
+    
+    def __str__(self):
+        return self.title
+
+class Queston(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    set = models.ForeignKey("Set", related_name="set_question", on_delete=models.CASCADE)
+    question = models.CharField(max_length=250,null=False)
+    created_at = models.DateTimeField(auto_now=True,)
+    updated_at = models.DateTimeField(auto_now=True,)
+    deleted_at = models.DateTimeField(auto_now=True,)
+
+    def __str__(self):
+        return self.question
+
+class Answers(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question = models.ForeignKey("Queston", related_name="question_answer", on_delete=models.CASCADE)
+    answer = models.CharField(max_length=250,null=False)
+    is_correct = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True,)
+    updated_at = models.DateTimeField(auto_now=True,)
+    deleted_at = models.DateTimeField(auto_now=True,)
+
+    def __str__(self):
+        return self.answer
